58eb5c5ae679aa383f7cf8c6f70b420aafd1b6de
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2018-06-28 08:37:38 -0700

    randomuser.me broke data; update tests so they work

--- test/functional.test.js
+++ test/functional.test.js
@@ -32,11 +32,7 @@ describe('fp-practice problems', () => {
     it('will calculate the age of each user', () => {
       expect(result.length).to.equal(5);
       expect(result).to.deep.equal([
-        23,
-        37,
-        62,
-        67,
-        48
+        21, 40, 58, 71, 23
       ]);
     });
   });
@@ -52,7 +48,7 @@ describe('fp-practice problems', () => {
     });
 
     it('will calculate the average age of the users', () => {
-      expect(result).to.equal(47.4);
+      expect(result).to.equal(42.6);
     });
   });
 
@@ -63,13 +59,11 @@ describe('fp-practice problems', () => {
       const inputData = await axios
         .get('http://randomuser.me/api/?results=5&seed=theSeed')
         .then(R.path(['data', 'results']));
-      console.log(inputData);
       result = getUsersWithBadPasswords(inputData);
     });
 
     it('will return the names of users with bad passwords (<7 characters)', () => {
-      console.log(result);
-      expect(result).to.deep.equal(['tinymouse202', 'crazyfrog635']);
+      expect(result).to.deep.equal(['sadwolf742', 'whitefrog500']);
     });
   });
 });
